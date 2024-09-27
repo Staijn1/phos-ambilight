f35da6f3ce4d907733b39bbd36fd210aea46ba99
Staijn1 <stein@jnkr.eu>
2024-09-27 12:58:27 +0200

    Add new avalonia project

---
+++ Phos.ScreenSync.New/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Phos.ScreenSync.New.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ Phos.ScreenSync.New/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Phos.ScreenSync.New.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
