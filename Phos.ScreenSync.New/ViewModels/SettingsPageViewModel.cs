be820cdd3fe472d4121db1d4c3a1a1cbd64d7fdb
Staijn1 <stein@jnkr.eu>
2024-09-28 13:04:31 +0200

    Implement settings page

--- Phos.ScreenSync.New/ViewModels/SettingsPageViewModel.cs
+++ Phos.ScreenSync.New/ViewModels/SettingsPageViewModel.cs
@@ -1,6 +1,24 @@
-﻿namespace Phos.ScreenSync.New.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
-public class SettingsPageViewModel: ViewModelBase
+namespace Phos.ScreenSync.New.ViewModels;
+
+public partial class SettingsPageViewModel: ViewModelBase
 {
-    
+    private readonly SettingsManager<UserSettings> _settingsManager;
+
+    [ObservableProperty]
+    private UserSettings _userSettings;
+
+    public SettingsPageViewModel()
+    {
+        _settingsManager = new SettingsManager<UserSettings>("UserSettings.json");
+        _userSettings = _settingsManager.LoadSettings() ?? new UserSettings();
+    }
+
+    [RelayCommand]
+    private void Save()
+    {
+        _settingsManager.SaveSettings(_userSettings);
+    }
 }
