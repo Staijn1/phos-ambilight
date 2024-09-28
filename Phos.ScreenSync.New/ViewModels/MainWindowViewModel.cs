889b04bc5a91adee0136a352595b9da0b27fa616
Staijn1 <stein@jnkr.eu>
2024-09-28 17:06:45 +0200

    Move websocket logic to viewmodel

--- Phos.ScreenSync.New/ViewModels/MainWindowViewModel.cs
+++ Phos.ScreenSync.New/ViewModels/MainWindowViewModel.cs
@@ -1,16 +1,36 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Phos.Connections;
+using Phos.Data;
+using SocketIOClient;
+using SocketIOClient.Transport;
 
 namespace Phos.ScreenSync.New.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
+    private readonly SettingsManager<UserSettings> _settingsManager;
+    private PhosSocketIOClient? _connection;
+    
     [ObservableProperty]
     private bool _isConnected;
 
     [ObservableProperty] 
     private ViewModelBase _currentScreen = new ScreenSyncPageViewModel();
 
+    public MainWindowViewModel()
+    {
+        _settingsManager = new SettingsManager<UserSettings>("userSettings.json");
+        var settings = _settingsManager.LoadSettings();
+
+        if (settings is { AutoConnect: true })
+        {
+            ConnectWebSocket(settings.WebSocketUrl);
+        }
+    }
+    
     [RelayCommand]
     public void NavigateToScreenSyncCommand()
     {
@@ -21,5 +41,38 @@ public partial class MainWindowViewModel : ViewModelBase
     public void NavigateToSettings()
     {
         CurrentScreen = new SettingsPageViewModel();
+    }
+    
+    private void ConnectWebSocket(string webSocketUrl)
+    {
+        var deviceName = "Phos Screensync - " + Environment.MachineName;
+
+        if (string.IsNullOrEmpty(webSocketUrl))
+        {
+            return;
+        }
+
+        _connection = new PhosSocketIOClient(webSocketUrl, new SocketIOOptions
+        {
+            Transport = TransportProtocol.WebSocket,
+            Query = new List<KeyValuePair<string, string>>
+            {
+                new("deviceName", deviceName),
+            }
+        });
+
+        _connection.OnConnect += async (sender, args) =>
+        {
+            IsConnected = true;
+            // First register ourselves as a user
+            await _connection.SendEvent(PhosSocketMessage.RegisterAsUser);
+            var response = await _connection.SendEvent(PhosSocketMessage.GetNetworkState);
+
+            var networkState = response.GetValue<NetworkState>();
+            // OnNewNetworkState(networkState);
+        };
+        
+        _connection.OnDisconnect += (sender, args) => { IsConnected = false; };
+        _connection.OnDatabaseChange += (sender, response) => { Console.WriteLine("Database change event received"); };
     }
 }
--- Phos.ScreenSync.New/Views/MainWindow.axaml.cs
+++ Phos.ScreenSync.New/Views/MainWindow.axaml.cs
@@ -10,50 +10,8 @@ namespace Phos.ScreenSync.New.Views;
 
 public partial class MainWindow : Window
 {
-    private readonly UserSettings? _settings;
-    private PhosSocketIOClient _connection;
-
     public MainWindow()
     {
         InitializeComponent();
-        SettingsManager<UserSettings> settingsManager = new("userSettings.json");
-
-        // Load the WebSocket URL from the settings
-        _settings = settingsManager.LoadSettings();
-
-        if (_settings is { AutoConnect: true })
-        {
-            ConnectWebSocket();
-        }
-    }
-
-    private void ConnectWebSocket()
-    {
-        var deviceName = "Phos Screensync - " + Environment.MachineName;
-
-        if (_settings?.WebSocketUrl == null)
-        {
-            return;
-        }
-
-        _connection = new PhosSocketIOClient(_settings?.WebSocketUrl, new SocketIOOptions
-        {
-            Transport = TransportProtocol.WebSocket,
-            Query = new List<KeyValuePair<string, string>>
-            {
-                new("deviceName", deviceName),
-            }
-        });
-
-        _connection.OnConnect += async (sender, args) =>
-        {
-            // First register ourselves as a user
-            await _connection.SendEvent(PhosSocketMessage.RegisterAsUser);
-            var response = await _connection.SendEvent(PhosSocketMessage.GetNetworkState);
-
-            var networkState = response.GetValue<NetworkState>();
-            // OnNewNetworkState(networkState);
-        };
-        _connection.OnDatabaseChange += (sender, response) => { Console.WriteLine("Database change event received"); };
     }
 }
